865b9d351dd303852560f8348e317f682b602e1b
Mariana Hristozkova <kukita@abv.bg>
2022-01-18 13:22:53 +0200

    Created BSTree and some methods

---
+++ BinarySearchTree/MyBSTree.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class MyBSTree
+    {
+        public class Node
+        {
+            public int value { set; get; }
+            public Node leftChild { set; get; }
+            public Node rightChild { set; get; }
+
+            public Node(int value)
+            {
+                this.value = value;
+            }
+
+            //to debug easy and see the values:
+             public override string ToString()
+            {
+                return "Node = " + value;
+            }
+        }
+
+        public Node Root {get; set; }
+
+        public void Insert(int value)
+        {
+            var node = new Node(value);
+
+            //two cases: if the tree is empty or is not
+            if (Root == null)
+            {
+                Root = node;
+                return;
+            }
+
+            var current = Root;
+            while (true)
+            {
+                if (value < current.value)    //we should go to the left subtree
+                {
+                    if (current.leftChild == null)
+                    {
+                        current.leftChild = node;
+                        break;
+                    }
+                    current = current.leftChild;
+                }
+                else
+                {
+                    if (current.rightChild == null)
+                    {
+                        current.rightChild = node;
+                        break;
+                    }
+                    current = current.rightChild;
+                }
+            }
+        }
+
+        public bool Find(int value)
+        {
+            var current = Root;
+
+            while(current != null)
+            {
+                if (value < current.value)
+                    current = current.leftChild;
+                else if (value > current.value)
+                    current = current.rightChild;
+                else
+                    return true;                
+            }
+
+            return false;
+        }
+
+        #region TraversePreOrder
+        public void TraversePreOrder()
+        {
+            TraversePreOrder(Root);
+        }
+        
+        private void TraversePreOrder(Node root)
+        {
+            if (root == null)
+                return;
+
+            Console.WriteLine(root.value);
+            TraversePreOrder(root.leftChild);
+            TraversePreOrder(root.rightChild);
+        }
+        #endregion
+
+        #region TraverseInOrder
+        public void TraverseInOrder()
+        {
+            TraverseInOrder(Root);
+        }
+
+        private void TraverseInOrder(Node root)
+        {
+            if (root == null)
+                return;
+
+            TraverseInOrder(root.leftChild);
+            Console.WriteLine(root.value);
+            TraverseInOrder(root.rightChild);
+        }
+        #endregion
+
+        #region TraversePostOrder
+        public void TraversePostOrder()
+        {
+            TraversePostOrder(Root);
+        }
+
+        private void TraversePostOrder(Node root)
+        {
+            if (root == null)
+                return;
+
+            TraversePostOrder(root.leftChild);
+            TraversePostOrder(root.rightChild);
+            Console.WriteLine(root.value);
+        }
+        #endregion
+
+        #region EqualityChecking
+        public bool Equals(MyBSTree other)
+        {
+            if (other == null)
+                return false;
+
+            return Equals(Root, other.Root);
+        }
+
+        private bool Equals(Node first, Node second)
+        {
+            if (first == null && second == null)
+                return true;
+
+            if (first != null && second != null)
+                return first.value == second.value
+                    && Equals(first.leftChild, second.leftChild)
+                    && Equals(first.rightChild, second.rightChild);
+
+            return false;
+        }
+        #endregion
+
+        #region Height 
+        public int Height()
+        {
+            return Height(Root);
+        }
+
+        private int Height(Node root)
+        {
+            if (root == null)
+                return -1;
+
+            if (root.leftChild == null && root.rightChild == null)
+                return 0;
+
+            return 1 + Math.Max(
+                Height(root.leftChild), 
+                Height(root.rightChild));
+        }
+        #endregion
+
+        #region DepthToNode-Get Nodes At Distance
+        public List<int> GetNodesAtDistance(int distance)
+        {
+            var list = new List<int>();
+            GetNodesAtDistance(Root, distance, list);
+            return list;
+        }
+
+        private void GetNodesAtDistance(Node root, int distance,List<int>list)
+        {
+            if (root == null)
+                return;
+
+            if(distance == 0)
+            {
+                list.Add(root.value);
+                return;
+            }
+
+            GetNodesAtDistance(root.leftChild, distance - 1,list);
+            GetNodesAtDistance(root.rightChild, distance - 1, list);
+        }
+
+        #endregion
+        
+        #region LevelOrderTraversal       
+
+        public void TraverseLevelOrder()
+        {
+            for (int i = 0; i < Height(); i++)
+            {
+                var list = GetNodesAtDistance(i);
+                foreach (var item in GetNodesAtDistance(i))
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+
+
+        #endregion
+
+
+        #region SizeOfTheTree
+        public int Size()
+        {
+            return Size(Root);
+        }
+
+        private int Size(Node root)
+        {
+            if (root == null)
+                return 0;
+
+            if (IsLeaf(root))
+                return 1;
+
+            return 1 + Size(root.leftChild) + Size(root.rightChild);
+        }
+
+        private bool IsLeaf(Node node)
+        {
+            return node.leftChild == null && node.rightChild == null;
+        }
+
+        #endregion
+
+        #region CountOfTheLeaves
+        public int CountLeaves()
+        {
+            return CountLeaves(Root);
+        }
+
+        private int CountLeaves(Node root)
+        {
+            if (root == null)
+                return 0;
+
+            if (IsLeaf(root))
+                return 1;
+
+            return CountLeaves(root.leftChild) + CountLeaves(root.rightChild);
+        }
+
+        #endregion
+
+
+
+
+
+        #region ValidatingBST
+        // we should check it a bynatry tree is a bynary Serac tree
+        // 1st  way - using recursion - for every node we visit it's subtrees -> if all the values are in the correct intervals, we are going to the next subtree
+        // 1st way is slow, because we are visiting every node multiply times
+
+        //2nd way - to traverse the tree and for every node we checks if the value of this node is in the correct interval
+
+        public bool IsBinarySearchTree()
+        {
+            return IsBinarySearchTree(Root, Int32.MinValue, Int32.MaxValue);
+        }
+
+        private bool IsBinarySearchTree(Node root, int min, int max)
+        {
+            if (root == null)
+                return true;
+
+            if (root.value < min || root.value > max)
+                return false;
+
+            return
+                IsBinarySearchTree(Root.leftChild, min, Root.value - 1)
+                && IsBinarySearchTree(Root.rightChild, Root.value + 1, max);
+        }
+        
+        #endregion
+    }
+}
--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -10,6 +10,46 @@ namespace BinarySearchTree
     {
         static void Main(string[] args)
         {
+            MyBSTree bsTree = new MyBSTree();
+            bsTree.Insert(7);
+            bsTree.Insert(4);
+            bsTree.Insert(9);
+            bsTree.Insert(1);
+            bsTree.Insert(6);
+            bsTree.Insert(8);
+            bsTree.Insert(10);
+
+            Console.WriteLine(bsTree.Find(1));
+            Console.WriteLine(bsTree.Find(111));
+
+            Console.WriteLine("Pre-Order Traversal:");
+            bsTree.TraversePreOrder();
+
+            Console.WriteLine("In-Order Traversal:");
+            bsTree.TraverseInOrder();
+
+            Console.WriteLine("Post-Order Traversal:");
+            bsTree.TraversePostOrder();
+
+            MyBSTree bsTree2 = new MyBSTree();
+            bsTree2.Insert(7);
+            bsTree2.Insert(4);
+            bsTree2.Insert(9);
+            bsTree2.Insert(1);
+            bsTree2.Insert(6);
+            bsTree2.Insert(8);
+            bsTree2.Insert(10);
+            bsTree2.Insert(12);
+
+            Console.WriteLine(bsTree.Equals(bsTree2));
+
+            Console.WriteLine(bsTree.Height());
+
+            bsTree2.TraverseLevelOrder();
+            
+            //Console.WriteLine(bsTree.IsBinarySearchTree());
+
+            Console.ReadKey();
         }
     }
 }
